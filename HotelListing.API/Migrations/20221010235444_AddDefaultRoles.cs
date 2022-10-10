fccfb27715dc8fa633264436995f9e7287207a87
Bret Kitchell <brittonashford@gmail.com>
2022-10-10 18:56:25 -0500

    Added default roles, CountryConfiguration and HotelConfiguration for seed data

---
+++ HotelListing.API/Migrations/20221010235444_AddDefaultRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class AddDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5247d3e6-9f98-456a-a935-b488be7bf233", "cb996540-14c7-4b0e-8541-92c367cd3f74", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "b8ff2c59-eb9a-44a1-a986-840e308add61", "070e94ca-dd0d-446a-a974-e31e494c955a", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5247d3e6-9f98-456a-a935-b488be7bf233");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b8ff2c59-eb9a-44a1-a986-840e308add61");
+        }
+    }
+}
